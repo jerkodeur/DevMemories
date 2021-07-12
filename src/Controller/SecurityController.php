d5e761f32b9a1c4504ea03d7e88fe1b182164001
jerkoLegion <jerome.potie@gmail.com>
2021-07-12 21:23:37 +0200

    Add flash messages for confirmed e-mail and register #7

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -20,10 +20,13 @@ class SecurityController extends AbstractController
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
+        if(isset($error)){
+            $this->addFlash('error', $error->getMessageKey());
+        }
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername]);
     }
 
     /**
