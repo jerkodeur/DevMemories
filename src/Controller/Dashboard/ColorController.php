c7489da0f73b64d6f354dfdf94ca3b69c215bc68
jerkoLegion <jerome.potie@gmail.com>
2021-07-23 12:09:42 +0200

    maj controller routes

--- src/Controller/Dashboard/ColorController.php
+++ src/Controller/Dashboard/ColorController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/dashboard/colors', name: 'dashboard_colors_')]
+#[Route('/dashboard/resources/colors', name: 'dashboard_resources_colors_')]
 class ColorController extends AbstractController
 {
     #[Route('/', name: 'list')]
