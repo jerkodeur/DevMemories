363558c384ab14329c7ba35702945f56ebed6831
jerkoLegion <jerome.potie@gmail.com>
2021-07-23 15:45:50 +0200

    Add a form for new categories and define async js for arrows #29"

--- src/Controller/Dashboard/CategoryController.php
+++ src/Controller/Dashboard/CategoryController.php
@@ -5,7 +5,7 @@ namespace App\Controller\Dashboard;
 use App\Entity\Category;
 use App\Entity\Color;
 use App\Form\CategoryType;
-use App\Form\ColorType;
+use App\Form\ColorsType;
 use App\Repository\CategoryRepository;
 use App\Repository\ColorRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,15 +18,15 @@ class CategoryController extends AbstractController
     #[Route('/', name: 'list')]
     public function index(CategoryRepository $categoryRepository, ColorRepository $colorRepository): Response
     {
-        // $color = new Color();
+        $color = new Color();
         $category = new Category();
-        // $color_form = $this->createForm(ColorType::class, $color);
+        $color_form = $this->createForm(ColorsType::class, $color);
         $category_form = $this->createForm(CategoryType::class, $category);
 
         return $this->render('dashboard/category/list.html.twig', [
             'categories' => $categoryRepository->findUserParentCategories($this->getUser()->getId()),
             'colors' => $colorRepository->findAll(),
-            // 'color_form' => $color_form->createView(),
+            'color_form' => $color_form->createView(),
             'category_form' => $category_form->createView()
         ]);
     }
