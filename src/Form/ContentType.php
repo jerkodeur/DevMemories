9d42b1be0266cf3dca8d74f2fc648f333ca02f89
jerkoLegion <jerome.potie@gmail.com>
2021-08-01 00:15:53 +0200

    Improve the contents form and create the delete route and some ajax operations to handle the published and private options #34

---
+++ src/Form/ContentType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Content;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('slug')
+            ->add('description')
+            ->add('content')
+            ->add('private')
+            ->add('published')
+            ->add('updated_at')
+            ->add('created_at')
+            ->add('type')
+            ->add('user')
+            ->add('category')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Content::class,
+        ]);
+    }
+}
