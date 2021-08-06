a6bc7d4bfcd670bbe4a9f0a1cc6b36ce17e120fa
jerkoLegion <jerome.potie@gmail.com>
2021-08-06 15:46:57 +0200

    Update readme file with some instructions

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,7 +7,7 @@ const $ = require('jquery');
 require('bootstrap');
 
 // any CSS you import will output into a single css file (app.css in this case)
-import Welcome from './Welcome';
+import Welcome from './components/Welcome';
 const imagesContext = require.context('../images', true, /\.(png|jpg|jpeg|gif|ico|svg|webp)$/);
 imagesContext.keys().forEach(imagesContext);
 
